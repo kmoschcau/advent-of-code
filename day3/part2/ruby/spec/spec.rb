e4f104940bf78c31928500378a487cd964f0a328
Kai Moschcau <mail@kmoschcau.de>
2021-12-03 23:39:04 +0100

    Add day 3 part 2

---
+++ day3/part2/ruby/spec/spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rspec'
+
+require_relative '../lib/d3p2'
+
+RSpec.describe '#calc_life_support' do
+  context 'when running the example' do
+    it 'has the correct depth' do
+      result = calc_life_support(%w[
+                                   00100
+                                   11110
+                                   10110
+                                   10111
+                                   10101
+                                   01111
+                                   00111
+                                   11100
+                                   10000
+                                   11001
+                                   00010
+                                   01010
+                                 ])
+      expect(result).to eq 230
+    end
+  end
+end
