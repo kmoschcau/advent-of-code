04a5f3897b2c07657e8cf42d0e420e8c80ac1e05
Kai Moschcau <mail@kmoschcau.de>
2021-12-04 17:59:40 +0100

    Reuse read_data method in day 4

--- 2021/ruby/lib/d4p2.rb
+++ 2021/ruby/lib/d4p2.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative 'd4p1'
 require_relative 'bingo_board'
 
 # Determine the winning board's product of unmarked numbers and last marked
@@ -18,20 +19,4 @@ def d4p2(lines)
   end
 
   0
-end
-
-# Read in the passed lines and create a draw order and boards.
-# @param lines [Array<String>] the file contents
-# @return [Array(Array<Integer>, Array<BingoBoard>)] the draw and boards
-def read_data(lines)
-  draw = lines.shift.split(',').map(&:to_i)
-  boards = []
-  until lines.empty?
-    if lines[0] == ''
-      lines.shift
-    else
-      boards << BingoBoard.new(lines.shift(5))
-    end
-  end
-  [draw, boards]
 end
