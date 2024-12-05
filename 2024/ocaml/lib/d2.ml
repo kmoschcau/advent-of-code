54737c77e9978b99fb213692c353ab3fd90224f5
Kai Moschcau <mail@kmoschcau.de>
2024-12-05 09:04:39 +0100

    feat: add day 2 part 2
    The tests for the example data succeed, but the result is still
    incorrect.

--- 2024/ocaml/lib/d2.ml
+++ 2024/ocaml/lib/d2.ml
@@ -10,33 +10,41 @@ let parse_lines lines = List.map (fun line -> parse_line line) lines
 (** Possible report states *)
 type report_state =
   | Indeterminate
-  | Initial of int
-  | Increasing of int
-  | Decreasing of int
-  | Unsafe
+  | Initial of { index : int; last : int }
+  | Increasing of { index : int; last : int }
+  | Decreasing of { index : int; last : int }
+  | Unsafe of { index : int }
+
+(** An accumulator for safety checks without tolerance *)
+let safety_check_acc state level =
+  match state with
+  | Indeterminate -> Initial { index = 0; last = level }
+  | Initial { index; last } ->
+      let next_index = index + 1 in
+      let diff = level - last in
+      if diff >= 1 && diff <= 3 then
+        Increasing { index = next_index; last = level }
+      else if diff >= -3 && diff <= -1 then
+        Decreasing { index = next_index; last = level }
+      else Unsafe { index = next_index }
+  | Increasing { index; last } ->
+      let next_index = index + 1 in
+      let diff = level - last in
+      if diff >= 1 && diff <= 3 then
+        Increasing { index = next_index; last = level }
+      else Unsafe { index = next_index }
+  | Decreasing { index; last } ->
+      let next_index = index + 1 in
+      let diff = last - level in
+      if diff >= 1 && diff <= 3 then
+        Decreasing { index = next_index; last = level }
+      else Unsafe { index = next_index }
+  | Unsafe { index } -> Unsafe { index }
 
 (** Check if the given report is safe. *)
 let is_report_safe report =
-  let state =
-    List.fold_left
-      (fun state level ->
-        match state with
-        | Indeterminate -> Initial level
-        | Initial last ->
-            let diff = level - last in
-            if diff >= 1 && diff <= 3 then Increasing level
-            else if diff >= -3 && diff <= -1 then Decreasing level
-            else Unsafe
-        | Increasing last ->
-            let diff = level - last in
-            if diff >= 1 && diff <= 3 then Increasing level else Unsafe
-        | Decreasing last ->
-            let diff = last - level in
-            if diff >= 1 && diff <= 3 then Decreasing level else Unsafe
-        | _ -> Unsafe)
-      Indeterminate report
-  in
-  match state with Unsafe -> false | _ -> true
+  let state = List.fold_left safety_check_acc Indeterminate report in
+  match state with Unsafe { index = _ } -> false | _ -> true
 
 (** Count all safe reports. *)
 let count_safe_reports reports =
@@ -52,7 +60,27 @@ let p1 input = split_lines input |> parse_lines |> count_safe_reports
 
 (* ========================================================================== *)
 
+(** Check if the given report is safe with tolerance. *)
+let is_report_safe_with_tolerance report =
+  let state = List.fold_left safety_check_acc Indeterminate report in
+  match state with
+  | Unsafe { index } -> (
+      let cleaned_report = List.filteri (fun i _ -> i <> index) report in
+      let cleaned_state =
+        List.fold_left safety_check_acc Indeterminate cleaned_report
+      in
+      match cleaned_state with Unsafe { index = _ } -> false | _ -> true)
+  | _ -> true
+
+(** Count all safe with tolerance reports. *)
+let count_safe_with_tolerance_reports reports =
+  List.fold_right
+    (fun report valid_count ->
+      match is_report_safe_with_tolerance report with
+      | false -> valid_count
+      | _ -> valid_count + 1)
+    reports 0
+
 (** Calculate the puzzle solution for part 2. *)
 let p2 input =
-  let _ = split_lines input in
-  0
+  split_lines input |> parse_lines |> count_safe_with_tolerance_reports
--- 2024/ocaml/test/test_d2.ml
+++ 2024/ocaml/test/test_d2.ml
@@ -28,6 +28,31 @@ let test_is_report_safe_6 () =
   (check bool) "when given 1 3 6 7 9" true (D2.is_report_safe [ 1; 3; 6; 7; 9 ])
 
 let test_d2p1 () = (check int) message 2 (d2p1 d2_test_input)
+
+let test_is_report_safe_with_tolerance_1 () =
+  (check bool) "when given 7 6 4 2 1" true
+    (D2.is_report_safe_with_tolerance [ 7; 6; 4; 2; 1 ])
+
+let test_is_report_safe_with_tolerance_2 () =
+  (check bool) "when given 1 2 7 8 9" false
+    (D2.is_report_safe_with_tolerance [ 1; 2; 7; 8; 9 ])
+
+let test_is_report_safe_with_tolerance_3 () =
+  (check bool) "when given 9 7 6 2 1" false
+    (D2.is_report_safe_with_tolerance [ 9; 7; 6; 2; 1 ])
+
+let test_is_report_safe_with_tolerance_4 () =
+  (check bool) "when given 1 3 2 4 5" true
+    (D2.is_report_safe_with_tolerance [ 1; 3; 2; 4; 5 ])
+
+let test_is_report_safe_with_tolerance_5 () =
+  (check bool) "when given 8 6 4 4 1" true
+    (D2.is_report_safe_with_tolerance [ 8; 6; 4; 4; 1 ])
+
+let test_is_report_safe_with_tolerance_6 () =
+  (check bool) "when given 1 3 6 7 9" true
+    (D2.is_report_safe_with_tolerance [ 1; 3; 6; 7; 9 ])
+
 let test_d2p2 () = (check int) message 4 (d2p2 d2_test_input)
 
 let () =
@@ -43,5 +68,26 @@ let () =
           ("Checks report safety correctly", `Quick, test_is_report_safe_6);
           ("Produces the correct result", `Quick, test_d2p1);
         ] );
-      ("Part 2", [ ("Produces the correct result", `Quick, test_d2p2) ]);
+      ( "Part 2",
+        [
+          ( "Checks report safety correctly with tolerance",
+            `Quick,
+            test_is_report_safe_with_tolerance_1 );
+          ( "Checks report safety correctly with tolerance",
+            `Quick,
+            test_is_report_safe_with_tolerance_2 );
+          ( "Checks report safety correctly with tolerance",
+            `Quick,
+            test_is_report_safe_with_tolerance_3 );
+          ( "Checks report safety correctly with tolerance",
+            `Quick,
+            test_is_report_safe_with_tolerance_4 );
+          ( "Checks report safety correctly with tolerance",
+            `Quick,
+            test_is_report_safe_with_tolerance_5 );
+          ( "Checks report safety correctly with tolerance",
+            `Quick,
+            test_is_report_safe_with_tolerance_6 );
+          ("Produces the correct result", `Quick, test_d2p2);
+        ] );
     ]
